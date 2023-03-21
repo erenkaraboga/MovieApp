138c5d8c13d6b35e5c1fd1e446ca15bd98f368fe
erenkaraboga <eren.karaboga@naylalabs.com>
2023-03-21 17:27:57 +0300

    Initial

---
+++ app/src/main/java/com/example/movie_application_eren_karaboga/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.movie_application_eren_karaboga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
