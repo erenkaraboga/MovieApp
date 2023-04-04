2bece3f5dc6064f5303b26d5d69a3a4e82de265c
erenkaraboga <eren.karaboga@naylalabs.com>
2023-04-05 00:01:57 +0300

    Add view binding to movie adapter

--- app/src/main/java/com/example/movie_application_eren_karaboga/presentation/movie/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/movie_application_eren_karaboga/presentation/movie/adapter/MovieAdapter.kt
@@ -1,58 +1,41 @@
 package com.example.movie_application_eren_karaboga.presentation.movie.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.movie_application_eren_karaboga.R
 import com.example.movie_application_eren_karaboga.base.utils.Constants
-import com.example.movie_application_eren_karaboga.domain.models.Movie
+import com.example.movie_application_eren_karaboga.databinding.MovieItemBinding
 import com.example.movie_application_eren_karaboga.domain.models.Result
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MyCustomHolder>() {
 
-    var liveData: List<Result>? = null;
+    private var liveData: List<Result>? = null
 
     fun setList(liveData: List<Result>) {
         this.liveData = liveData
         notifyDataSetChanged()
     }
 
-    class MyCustomHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val movieName = view.findViewById<TextView>(R.id.movieNameTv)
-        val genre = view.findViewById<TextView>(R.id.genreTv)
-        val duration = view.findViewById<TextView>(R.id.durationTv)
-        val rate = view.findViewById<TextView>(R.id.ratingTv)
-        val posterView = view.findViewById<ImageView>(R.id.posterIv)
-
-
+    inner class MyCustomHolder(private val binding: MovieItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(data: Result) {
-            movieName.text = data.title
-            duration.text = data.releaseDate
-            rate.text = data.voteAverage.toString()
-            genre.text = data.originalLanguage
-            Glide.with(posterView).load(Constants.posterUrl + data.posterPath)
-                .into(posterView)
+            binding.movieNameTv.text = data.title
+            binding.durationTv.text = data.releaseDate
+            binding.ratingTv.text = data.voteAverage.toString()
+            binding.genreTv.text = data.originalLanguage
+            Glide.with(binding.posterIv).load(Constants.posterUrl + data.posterPath).into(binding.posterIv)
         }
     }
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapter.MyCustomHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
-        return MyCustomHolder(view)
+        val binding = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyCustomHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieAdapter.MyCustomHolder, position: Int) {
-        holder.bind(liveData!!.get(position))
+        liveData?.get(position)?.let { holder.bind(it) }
     }
 
-    override fun getItemCount(): Int {
-        if (liveData == null) {
-            return 0
-        } else return liveData!!.size
-    }
+    override fun getItemCount(): Int = liveData?.size ?: 0
 }
