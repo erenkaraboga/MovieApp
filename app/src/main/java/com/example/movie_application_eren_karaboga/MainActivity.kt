4f9588b554690915be3653928d605f15369c84c7
erenkaraboga <eren.karaboga@naylalabs.com>
2023-04-04 23:52:23 +0300

    Add view binding

--- app/src/main/java/com/example/movie_application_eren_karaboga/MainActivity.kt
+++ app/src/main/java/com/example/movie_application_eren_karaboga/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.movie_application_eren_karaboga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.movie_application_eren_karaboga.databinding.ActivityMainBinding
 import com.example.movie_application_eren_karaboga.domain.models.Movie
+import com.example.movie_application_eren_karaboga.presentation.movie.MovieFragment
 import com.example.movie_application_eren_karaboga.presentation.movie.MovieViewModel
 import com.example.movie_application_eren_karaboga.presentation.movie.adapter.MovieAdapter
 import dagger.hilt.EntryPoint
@@ -16,27 +20,19 @@ import retrofit2.Call
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     private  lateinit var  movieAdapter : MovieAdapter
     val viewModel by lazy {
          ViewModelProvider(this,defaultViewModelProviderFactory).get(MovieViewModel::class.java)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val verticalList = LinearLayoutManager(applicationContext,LinearLayoutManager.VERTICAL,false)
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val movieFragment = MovieFragment()
+        val fragmentManager : FragmentManager=supportFragmentManager
+        val fragmentTransaction:FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.activity_main,movieFragment).commit()
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        movieAdapter=MovieAdapter()
-        recyclerView.adapter =movieAdapter
-        recyclerView.layoutManager = verticalList
-        viewModel.getObserverLiveData().observe(this, object: Observer<Movie> {
-            override fun onChanged(t: Movie?) {
-                if(t!=null){
-                    movieAdapter.setList(t.results);
-                }
-            }
-
-        })
-        viewModel.loadPopularData("1");
     }
 }
--- app/src/main/java/com/example/movie_application_eren_karaboga/presentation/movie/MovieFragment.kt
+++ app/src/main/java/com/example/movie_application_eren_karaboga/presentation/movie/MovieFragment.kt
@@ -5,23 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movie_application_eren_karaboga.R
+import com.example.movie_application_eren_karaboga.databinding.FragmentMovieBinding
+import com.example.movie_application_eren_karaboga.domain.models.Movie
+import com.example.movie_application_eren_karaboga.presentation.movie.adapter.MovieAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private var _binding : FragmentMovieBinding ? = null
+    private val binding get() = _binding!!
+    private  lateinit var  movieAdapter : MovieAdapter
+    val viewModel by lazy {
+        ViewModelProvider(this,defaultViewModelProviderFactory).get(MovieViewModel::class.java)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        _binding = FragmentMovieBinding.inflate(inflater,container,false)
+
+        _binding = FragmentMovieBinding.inflate(inflater,container,false)
+
+        val verticalList = LinearLayoutManager(context,
+            LinearLayoutManager.VERTICAL,false)
+        val recyclerView = binding.recyclerView
+        movieAdapter=MovieAdapter()
+        recyclerView.adapter =movieAdapter
+        recyclerView.layoutManager = verticalList
+        viewModel.getObserverLiveData().observe(viewLifecycleOwner, object: Observer<Movie> {
+            override fun onChanged(t: Movie?) {
+                if(t!=null){
+                    movieAdapter.setList(t.results);
+                }
+            }
+
+        })
+        viewModel.loadPopularData("1");
+        return binding.root
     }
 
 
